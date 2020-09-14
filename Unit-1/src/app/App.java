daa734acea27327f9edd80e20a493fc728deea59
HamiltonFelling <832419@mystma.org>
2020-09-14 18:45:22 +0000

    added top 5 movies

--- Unit-1/src/app/App.java
+++ Unit-1/src/app/App.java
@@ -2,6 +2,10 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Lol");
+        System.out.println("1. A Silent Voice");
+        System.out.println("2. Maquia");
+        System.out.println("3. The Last: Naruto the Movie");
+        System.out.println("4. End Game");
+        System.out.println("5. The Spiderman Movies ");
     }
 }
