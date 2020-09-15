cbcac2c37ff126bb6347240c4777d17bd19376fc
HamiltonFelling <832419@mystma.org>
2020-09-15 20:19:21 +0000

    Variables

--- Unit-1/src/app/App.java
+++ Unit-1/src/app/App.java
@@ -2,10 +2,36 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1. A Silent Voice");
-        System.out.println("2. Maquia");
-        System.out.println("3. The Last: Naruto the Movie");
-        System.out.println("4. End Game");
-        System.out.println("5. The Spiderman Movies ");
-    }
+        boolean livesInAlbertville = true;
+
+        System.out.println("Do I live in Albertville?");
+        System.out.println(livesInAlbertville);
+        
+        byte hamilton = 16;
+        System.out.println("How old is Hamilton");
+        System.out.println(hamilton);
+    
+        char cool = 'A';
+        System.out.println("What is the first letter in the alphabet?");
+        System.out.println(cool);
+    
+        short lol = 750;
+        System.out.println("What is 500+250");
+        System.out.println(lol);
+    
+        int rad = 1000000;
+        System.out.println("What is  2000000/2");
+        System.out.println(rad);
+    
+        long pool = 7000000000L;
+        System.out.println("How much people are in the world");
+        System.out.println(pool);
+    
+        float ouch = 1.5F;
+        System.out.println("What is 1+0.5");
+        System.out.println(ouch);
+    
+        double poop = 1.999999999999999D;
+        System.out.println("What os 1+.999999999999999");
+        System.out.println(poop);}
 }
