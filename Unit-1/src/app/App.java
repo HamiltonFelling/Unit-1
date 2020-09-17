1fa0e08dc29c1e2e5af3c0ab320accbb944d14f3
HamiltonFelling <832419@mystma.org>
2020-09-17 20:07:37 +0000

    Completed Mad Lib Assignment

--- Unit-1/src/app/App.java
+++ Unit-1/src/app/App.java
@@ -2,36 +2,21 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        boolean livesInAlbertville = true;
+        String person1 = "Hamilton";
+        String verb1 = "play video games";
+        String dayOfWeek = "saturday";
+        String adjective1 = "tasty";
+        String food = "ramen";
+        String verb2 = "walk";
+        String place1 = "the park";
+        String person2 = "his mom";
+        String adjective2 = "good";
+        String place2 = "resteraunt";
+        String verb3 = "eat";
 
-        System.out.println("Do I live in Albertville?");
-        System.out.println(livesInAlbertville);
+        System.out.println(person1 + " is a programmer who likes to " + verb1 + ".");
+        System.out.println("Every " + dayOfWeek + " he makes a bowl of " + adjective1 + " " + food + " for breakfast and goes on a " + verb2 + " at " + place1 + " with " + person2 + ".");
+        System.out.println("Sometimes, he go to a " + adjective2 + " " + place2 + " with " + person2 + " so they can " + verb3 + " together.");
         
-        byte hamilton = 16;
-        System.out.println("How old is Hamilton");
-        System.out.println(hamilton);
-    
-        char cool = 'A';
-        System.out.println("What is the first letter in the alphabet?");
-        System.out.println(cool);
-    
-        short lol = 750;
-        System.out.println("What is 500+250");
-        System.out.println(lol);
-    
-        int rad = 1000000;
-        System.out.println("What is  2000000/2");
-        System.out.println(rad);
-    
-        long pool = 7000000000L;
-        System.out.println("How much people are in the world");
-        System.out.println(pool);
-    
-        float ouch = 1.5F;
-        System.out.println("What is 1+0.5");
-        System.out.println(ouch);
-    
-        double poop = 1.999999999999999D;
-        System.out.println("What os 1+.999999999999999");
-        System.out.println(poop);}
+    }
 }
