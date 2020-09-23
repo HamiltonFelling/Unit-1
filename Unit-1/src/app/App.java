1091fe30156f10ac5803b5a10e7376e8c2eed324
HamiltonFelling <832419@mystma.org>
2020-09-23 18:26:31 +0000

    Completed Color Assignment

--- Unit-1/src/app/App.java
+++ Unit-1/src/app/App.java
@@ -9,11 +9,14 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter number of times you want the color purple repeated");
+        System.out.println("Please enter a color");
+        String color = input.next();
+
+        System.out.println("Please enter number of times you want the color repeated");
         int number = input.nextInt();
 
         while (count < number) {
-            System.out.println("purple");
+            System.out.println(color);
             count = count + 1;
         }
             
