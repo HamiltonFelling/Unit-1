7fae05902776363a65fc05aebb25cae72eacac56
HamiltonFelling <832419@mystma.org>
2020-09-25 17:30:47 +0000

    Finished 9-25 assessment

--- Unit-1/src/app/App.java
+++ Unit-1/src/app/App.java
@@ -5,22 +5,25 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int count = 0;
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Please enter a color");
-        String color = input.next();
-
-        System.out.println("Please enter number of times you want the color repeated");
-        int number = input.nextInt();
-
-        while (count < number) {
-            System.out.println(color);
-            count = count + 1;
-        }
-            
-    
         
+        for(int i =10; i <= 20; i = i +1){
+            System.out.println("The count is " + i); 
+        }
+
+         for(int a =10; a <= 100; a = a +1){
+            System.out.println("The count is " + a);
+        }
+
+         for(int b =1000; b <= 256000; b = b +1){
+            System.out.println("The count is " + b);
+        }
+
+        for(int c =11; c <= 12; c = c +1){
+            System.out.println("The count is " + c);
+        }
+
+        for(int d =10; d >= 0; d = d +1){
+            System.out.println("The count is " + d);
+        }
     }
 }
