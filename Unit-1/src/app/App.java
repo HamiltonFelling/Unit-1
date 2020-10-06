30c6bcf9e6bb410b06fd03a5315c62c7a2a48c66
HamiltonFelling <832419@mystma.org>
2020-10-06 03:19:44 +0000

    Completed Challenge 1 & 2

--- Unit-1/src/app/App.java
+++ Unit-1/src/app/App.java
@@ -9,9 +9,34 @@ public class App {
         Scanner input = new Scanner(System.in);
 
         for (int pp = -1; pp < 1000;) {
-        pp = pp + 1;
+            pp = pp + 1;
             System.out.println(pp);
+        }
 
+        int password = 2132;
+        int tt = 2;
+        int qq = 0;
+        int gg = 0;
+
+        System.out.println("Please enter password, you have 3 Attempts");
+
+        while (qq < 4) {
+            int rr = input.nextInt();
+
+            if (rr == password) {
+                System.out.println("Correct password");
+                System.out.println("Hello Mr. Osowski");
+            } else if (gg < 2) {
+                System.out.println("Please retry password " + tt + " attempt's' remaining.");
+                qq = qq + 1;
+                tt = tt - 1;
+                gg = gg + 1;
+            } else {
+                qq = qq + 1;
+            }
+            if (qq == 3) {
+                System.out.println("Error: too many attempts, please try again later.");
+            }
         }
 
     }
