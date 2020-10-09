ada0904f044bf0fae2014646943d7a83c3edba8e
HamiltonFelling <832419@mystma.org>
2020-10-09 20:48:37 +0000

    Completed Challenge 3

--- Unit-1/src/app/App.java
+++ Unit-1/src/app/App.java
@@ -8,35 +8,23 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
-        for (int pp = -1; pp < 1000;) {
-            pp = pp + 1;
-            System.out.println(pp);
+        int pp = 1;
+
+        System.out.println("Please guess the number 1-100");
+        while (pp < 2) {
+        int rr = input.nextInt();
+
+        if (rr == 54) {
+            System.out.println("You have guessed the right number '54'.");
+            pp = pp + 2;
+        }
+        else if (rr <= 53) {
+            System.out.println("Low");
+        }
+        else if (rr >= 55) {
+            System.out.println("High");
         }
 
-        int password = 2132;
-        int tt = 2;
-        int qq = 0;
-        int gg = 0;
-
-        System.out.println("Please enter password, you have 3 Attempts");
-
-        while (qq < 4) {
-            int rr = input.nextInt();
-
-            if (rr == password) {
-                System.out.println("Correct password");
-                System.out.println("Hello Mr. Osowski");
-            } else if (gg < 2) {
-                System.out.println("Please retry password " + tt + " attempt's' remaining.");
-                qq = qq + 1;
-                tt = tt - 1;
-                gg = gg + 1;
-            } else {
-                qq = qq + 1;
-            }
-            if (qq == 3) {
-                System.out.println("Error: too many attempts, please try again later.");
-            }
         }
 
     }
