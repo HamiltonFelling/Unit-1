12570c74331c611aa2214b8a60678256ebb4c35a
HamiltonFelling <832419@mystma.org>
2020-10-20 18:07:56 +0000

    Progress

--- Unit-1/src/app/App.java
+++ Unit-1/src/app/App.java
@@ -13,26 +13,34 @@ public class App {
         int a = 0;
         String inside = "inside";
         String outside = "outside";
-        int c = 1;
-
-        
+        int c = 5;
+        int j = 0;
 
         while (a < 10) {
             System.out.println("Would you like to make an inside or outside bet?");
             String b = input.nextLine();
             if (b.equals(inside)) {
-                while (b =)
-                System.out.println("Please enter the number you want to bet on '1 - 36'");
-                String d = input.nextLine();
-                
-            } else if (b.equals(outside)) {
-            } else {
-                System.out.println("invalid statement, Please try again.");
-            }
-            }
+                System.out.println("What number would you like to bet on");
+                int g = input.nextInt();
+                System.out.println("How much would you like to bet");
+                int h = input.nextInt();
+                if (h <= c){
+                    System.out.println("Would you like to make another bet? yes or no");
+                    String i = input.nextLine();
+                    if (i.equals("yes")) {
+                        a = a + 1;
+                    } else if (i.equals("no")) {
+                        j = j + 1;
+                    } else {
+                        System.out.println("invalid statement, Please try again.");
+                    }
+                } else {
 
+                }
+                } else if (b.equals(outside)) {
+                } else {
+                    System.out.println("invalid statement, Please try again.");
+            }
         }
-
     }
-
 }
