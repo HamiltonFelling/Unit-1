05ee4bb3f0483898e791c1207f328f026ac0e01b
HamiltonFelling <832419@mystma.org>
2020-10-20 20:14:57 +0000

    Progress

--- Unit-1/src/app/App.java
+++ Unit-1/src/app/App.java
@@ -16,7 +16,12 @@ public class App {
         int c = 5;
         int j = 0;
 
-        while (a < 10 && a > 0) {
+        int aa = 0;
+        int bb = 0;
+        int cc = 0;
+        int dd = 0;
+
+        while (a <= 10 && a > 0) {
 
             System.out.println("Would you like to make an inside or outside bet?");
             String b = input.nextLine();
@@ -35,26 +40,32 @@ public class App {
                         j = j + 1;
 
                     } else if (i.equals("no")) {
-
+                        System.out.println(aa);
+                        System.out.println(bb);
+                        System.out.println(cc);
+                        System.out.println(dd);
                     } else {
                         System.out.println("invalid statement, Please try again.");
                     }
+
+                        if (j == 1){
+                         aa = g;
+                         bb = h;
+            }
+                         if (j == 2){
+                         cc = g;
+                         dd = h;
+            }
                 } else {
-                        System.out.println("invalid statement, Please try again.");
+                        System.out.println("You do not have enough balance, Immediate cash out.");
+                        a = 100;
               }
          
              } else if (b.equals(outside)) {
              } else {
                     System.out.println("invalid statement, Please try again.");
             }
-            if (j == 1){
-                int aa = g;
-                int bb = h;
-            }
-            if (j == 2){
-                int cc = g;
-                int dd = h;
-            }
+
 
 
         
