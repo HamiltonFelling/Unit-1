2751c84eaed691fb28508b0ba02ddbcafd89c422
HamiltonFelling <832419@mystma.org>
2020-10-20 19:38:28 +0000

    Progress

--- Unit-1/src/app/App.java
+++ Unit-1/src/app/App.java
@@ -10,37 +10,54 @@ public class App {
 
         System.out.println("Welcome to Roulette");
         System.out.println("ROUND 1");
-        int a = 0;
+        int a = 10;
         String inside = "inside";
         String outside = "outside";
         int c = 5;
         int j = 0;
 
-        while (a < 10) {
+        while (a < 10 && a > 0) {
+
             System.out.println("Would you like to make an inside or outside bet?");
             String b = input.nextLine();
+
             if (b.equals(inside)) {
                 System.out.println("What number would you like to bet on");
                 int g = input.nextInt();
                 System.out.println("How much would you like to bet");
                 int h = input.nextInt();
+                
                 if (h <= c){
                     System.out.println("Would you like to make another bet? yes or no");
                     String i = input.nextLine();
                     if (i.equals("yes")) {
-                        a = a + 1;
-                    } else if (i.equals("no")) {
+                        a = a - 1;
                         j = j + 1;
+
+                    } else if (i.equals("no")) {
+
                     } else {
                         System.out.println("invalid statement, Please try again.");
                     }
                 } else {
-
-                }
-                } else if (b.equals(outside)) {
-                } else {
+                        System.out.println("invalid statement, Please try again.");
+              }
+         
+             } else if (b.equals(outside)) {
+             } else {
                     System.out.println("invalid statement, Please try again.");
             }
+            if (j == 1){
+                int aa = g;
+                int bb = h;
+            }
+            if (j == 2){
+                int cc = g;
+                int dd = h;
+            }
+
+
+        
         }
     }
 }
