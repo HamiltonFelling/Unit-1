f3a8e839b321f0206adbfc86dbfcc3befe9a1af2
HamiltonFelling <832419@mystma.org>
2020-10-26 19:28:44 +0000

    Progress

--- Unit-1/src/app/App.java
+++ Unit-1/src/app/App.java
@@ -10,16 +10,16 @@ public class App {
 
         System.out.println("Welcome to Roulette");
         System.out.println("ROUND 1");
+        
         int a = 10;
+        int c = 5;
+
         String inside = "inside";
         String outside = "outside";
-        int c = 5;
-        int j = 0;
 
-        int aa = 0;
-        int bb = 0;
-        int cc = 0;
-        int dd = 0;
+        int max = 36;
+        int min = 0;
+        int range = max - min + 1;
 
         while (a <= 10 && a > 0) {
 
@@ -31,31 +31,41 @@ public class App {
                 int g = input.nextInt();
                 System.out.println("How much would you like to bet");
                 int h = input.nextInt();
+
+                int rand = (int)(Math.random()* range) + max;
                 
                 if (h <= c){
                     System.out.println("Would you like to make another bet? yes or no");
                     String i = input.nextLine();
                     if (i.equals("yes")) {
-                        a = a - 1;
-                        j = j + 1;
+
+
+
+
+
+
+
+
+
+
 
                     } else if (i.equals("no")) {
-                        System.out.println(aa);
-                        System.out.println(bb);
-                        System.out.println(cc);
-                        System.out.println(dd);
                     } else {
                         System.out.println("invalid statement, Please try again.");
                     }
 
-                        if (j == 1){
-                         aa = g;
-                         bb = h;
-            }
-                         if (j == 2){
-                         cc = g;
-                         dd = h;
-            }
+
+
+
+
+
+
+
+
+
+
+
+
                 } else {
                         System.out.println("You do not have enough balance, Immediate cash out.");
                         a = 100;
